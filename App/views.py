f215cc4dce27e6ab977f9b35a9f61bd1281c3dc0
Vithugsil <vitorhugosilva012@gmail.com>
2025-09-01 17:07:58 -0300

    filtroFeito

--- App/views.py
+++ App/views.py
@@ -11,12 +11,24 @@ def home(request):
     
     print("User is logged in with user_id:", request.session['user_id'])
 
+    # verificar o poprque nao ta filtrando
+    Status_id = request.POST.get('status', 0)
+
+    print("Filtering tasks with Status_id:", Status_id)
+
     user = User.objects.get(id=request.session['user_id'])
-    tasks = Task.objects.filter(user=user)
+
+    if Status_id != "0":
+        tasks = Task.objects.filter(user=user, status__id=Status_id).all()
+    else:
+        tasks = Task.objects.filter(user=user).all()
+    
+    task_status = Status.objects.all()
 
     context = {
         "current_user": User,
-        "user_tasks": tasks
+        "user_tasks": tasks,
+        "task_status": task_status
     }
 
     return render(request, 'home.html', context)
@@ -77,12 +89,13 @@ def newTask(request):
         return redirect('login')
     
     task_status = Status.objects.all()
+    task_users = User.objects.all()
 
     if request.method == 'POST':
         title = request.POST.get('title')
         status = Status.objects.get(id=request.POST.get('status'))
         description = request.POST.get('description')
-        user = User.objects.get(id=request.session['user_id'])
+        user = User.objects.get(id=request.POST.get('user'))
 
         task, create = Task.objects.get_or_create(title=title, status=status, description=description, user=user)
 
@@ -95,10 +108,12 @@ def newTask(request):
         return redirect('home')
 
     context = {
-        'task_status': task_status
+        'task_status': task_status,
+        'newTaskPage': True,
+        'task_users': task_users
     }
 
-    return render(request, 'newTask.html', context)
+    return render(request, 'Task.html', context)
 
 def deleteTask(request, task_id):
     if 'user_id' not in request.session:
@@ -112,3 +127,35 @@ def deleteTask(request, task_id):
         print("Task not found or does not belong to the user")
 
     return redirect('home')
+
+def editTask(request, task_id):
+    if 'user_id' not in request.session:
+        print("User not logged in, redirecting to login page")
+        return redirect('login')
+
+    task_status = Status.objects.all()
+    task_users = User.objects.all()
+
+    try:
+        task = Task.objects.get(id=task_id, user__id=request.session['user_id'])
+    except Task.DoesNotExist:
+        print("Task not found or does not belong to the user")
+        return redirect('home')
+
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.status = Status.objects.get(id=request.POST.get('status'))
+        task.user = User.objects.get(id=request.POST.get('user'))
+        task.description = request.POST.get('description')
+        task.save()
+        print("Task updated:", task.title)
+        return redirect('home')
+
+    context = {
+        'task': task,
+        'task_status': task_status,
+        'newTaskPage': False,
+        'task_users': task_users
+    }
+
+    return render(request, 'Task.html', context)
